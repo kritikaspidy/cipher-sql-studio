129c532b04e106ed07143acda00f4ac2014298a6
Kritika Garg <kritikagarg@Kritikas-MacBook-Air.local>
2026-03-27 13:50:58 +0530

    fix production

--- frontend/cipher-sql/src/api/hintapi.js
+++ frontend/cipher-sql/src/api/hintapi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api"
+  // baseURL: "http://localhost:8000/api"
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+
 });
 
 export const getHint = (data) => API.post("/hint", data);
